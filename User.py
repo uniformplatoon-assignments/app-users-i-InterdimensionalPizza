1a932a7144a07a2cbe0dbad74d753ae2efc13c1a
interdimensionalpizza <michaellake05@gmail.com>
2023-06-13 10:36:42 -0700

    finished assignment

--- User.py
+++ User.py
@@ -1,1 +1,14 @@
-# your User class goes here
+class Users:
+    def __init__(self, name, email_address, drivers_license_num ):
+        self.name = name
+        self.email_address = email_address
+        self.drivers_license_num = drivers_license_num
+    
+    def __str__(self):
+        return f'{self.name} {self.email_address} {self.drivers_license_num}'
+    
+Greg = Users('Greg', 'greg@codeplatoon.org', '123456789')
+Matt = Users('Matt', 'matt@hotmail.com', '1234567')
+
+print(Greg)
+print(Matt)
